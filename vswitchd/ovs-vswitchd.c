b4c6b1571ba439f730a0eb59f8b187f215846c4c
Shad Ansari <shad.ansari@hpe.com>
2016-01-28 17:43:57 +0000

    Upgrade ops-ovs to OVS 2.5
    Change-Id: Ie3210c65ae687cafb1e0380cd03a857752997cfc
    Signed-off-by: Shad Ansari <shad.ansari@hpe.com>

--- vswitchd/ovs-vswitchd.c
+++ vswitchd/ovs-vswitchd.c
@@ -83,7 +83,7 @@ main(int argc, char *argv[])
     argc -= retval;
     argv += retval;
 
-    proctitle_init(argc, argv);
+    ovs_cmdl_proctitle_init(argc, argv);
     service_start(&argc, &argv);
     remote = parse_options(argc, argv, &unixctl_path, &plugins_path);
 
@@ -201,7 +201,7 @@ parse_options(int argc, char *argv[], char **unixctl_pathp, char **plugins_pathp
         {"dpdk", required_argument, NULL, OPT_DPDK},
         {NULL, 0, NULL, 0},
     };
-    char *short_options = long_options_to_short_options(long_options);
+    char *short_options = ovs_cmdl_long_options_to_short_options(long_options);
 
     for (;;) {
         int c;
@@ -244,7 +244,7 @@ parse_options(int argc, char *argv[], char **unixctl_pathp, char **plugins_pathp
             break;
 
         case OPT_ENABLE_DUMMY:
-            dummy_enable(optarg && !strcmp(optarg, "override"));
+            dummy_enable(optarg);
             break;
 
         case OPT_DISABLE_SYSTEM:
