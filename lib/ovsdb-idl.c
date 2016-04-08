ad4e8e82b5293e396cfede613c229fd7aa8b4de1
Esteban Rodriguez Betancourt <estebarb@hpe.com>
2016-04-08 15:36:51 -0600

    new: dev: OVSDB Priority Sessions
    The priority sessions feature adds to the OVSDB Server
    the ability to identify and prioritize client sessions.
    
    The clients identify themselves using a new JSON RPC command,
    "identify", which receives as a parameter the name or identifier
    of the client.
    
    Then the server compares that identifier with a list of
    priorities assigned by name. If the name matches, then that
    priority is selected, otherwise a default priority is assigned.
    
    The priorities are saved in a json file, that the ovsdb server
    receives as a optional parameter when initializing. With
    ovsdb-appctl the ovsdb-server can be forced to reload that file
    in runtime.
    
    The priorities goes from 0 to 15, being 0 the most important,
    and 15 the less important.
    
    The OVSDB Server process the requests in a loop. The priority
    system works deferring the processing of the requests with a
    lower priority to a next iteration of the loop.
    
    The sessions with priority 0 run all the times (16 times of each
    16 iterations), with priority 1 run 15 times of each 16 iterations,
    and so on, finally with priority 15, that runs just 1 time of each
    16 iterations.
    
    The JSON RPC command was added to ovsdb-client too.
    
    Change-Id: I8b9740be26967d8dec6737c62b90b7e0ea0e3039
    Signed-off-by: Esteban Rodriguez Betancourt <estebarb@hpe.com>

--- lib/ovsdb-idl.c
+++ lib/ovsdb-idl.c
@@ -52,6 +52,8 @@ COVERAGE_DEFINE(txn_try_again);
 COVERAGE_DEFINE(txn_not_locked);
 COVERAGE_DEFINE(txn_error);
 
+#define UNDEFINED_PRIORITY -1
+
 /* An arc from one idl_row to another.  When row A contains a UUID that
  * references row B, this is represented by an arc from A (the source) to B
  * (the destination).
@@ -79,7 +81,8 @@ struct ovsdb_idl_arc {
 enum ovsdb_idl_state {
     IDL_S_SCHEMA_REQUESTED,
     IDL_S_MONITOR_REQUESTED,
-    IDL_S_MONITORING
+    IDL_S_MONITORING,
+    IDL_S_IDENTIFY
 };
 
 struct ovsdb_idl {
@@ -94,6 +97,7 @@ struct ovsdb_idl {
     unsigned int state_seqno;
     enum ovsdb_idl_state state;
     struct json *request_id;
+    uint8_t session_priority;
 
     /* Database locking. */
     char *lock_name;            /* Name of lock we need, NULL if none. */
@@ -215,6 +219,7 @@ ovsdb_idl_create(const char *remote, const struct ovsdb_idl_class *class,
     idl = xzalloc(sizeof *idl);
     idl->class = class;
     idl->session = jsonrpc_session_open(remote, retry);
+    idl->session_priority = IDL_PRIORITY_UNDEFINED;
     shash_init(&idl->table_by_name);
     idl->tables = xmalloc(class->n_tables * sizeof *idl->tables);
     for (i = 0; i < class->n_tables; i++) {
@@ -321,6 +326,28 @@ ovsdb_idl_clear(struct ovsdb_idl *idl)
     }
 }
 
+/* Identifies the client within the OVSDB Server */
+void
+ovsdb_idl_set_identity(struct ovsdb_idl *idl, char* name)
+{
+    struct jsonrpc_msg *msg;
+
+    json_destroy(idl->request_id);
+    msg = jsonrpc_create_request(
+        "identify",
+        json_array_create_1(json_string_create(name)),
+        &idl->request_id);
+    idl->state = IDL_S_IDENTIFY;
+    jsonrpc_session_send(idl->session, msg);
+}
+
+/* Returns the current priority assigned to the IDL session */
+uint8_t
+ovsdb_idl_get_priority(struct ovsdb_idl *idl)
+{
+    return idl->session_priority;
+}
+
 /* Processes a batch of messages from the database server on 'idl'.  This may
  * cause the IDL's contents to change.  The client may check for that with
  * ovsdb_idl_get_seqno(). */
@@ -382,7 +409,13 @@ ovsdb_idl_run(struct ovsdb_idl *idl)
                 ovsdb_idl_clear(idl);
                 ovsdb_idl_parse_update(idl, msg->result);
                 break;
-
+            case IDL_S_IDENTIFY:
+                if(msg->result->type == JSON_ARRAY
+                   && msg->result->u.array.n == 1
+                   && msg->result->u.array.elems[0]->type == JSON_INTEGER){
+                    idl->session_priority = msg->result->u.array.elems[0]->u.integer;
+                }
+                break;
             case IDL_S_MONITORING:
             default:
                 OVS_NOT_REACHED();
