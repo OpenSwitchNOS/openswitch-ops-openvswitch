021a041f9ae594c0489a6918dba013f5f66ab852
arnoldo.lutz.guevara@hpe.com <arnoldo.lutz.guevara@hpe.com>
2016-05-02 08:31:16 -0700

    Enabling Open Vswitch tests inside OpenSwitch
    This changes enable the compiling of Open vSwitch tests inside
    OPS-BUILD.
    The tests related to Open vSwitch modules that aren't compiled in
    ops-build are excluded from the code generation and some tests that
    depend on ovs-vswitchd are skipped. The files remain in the folder.
    
    Check file  lib/socket-util-unix.c for a pending condition that need to be
    defined to run the tests in native host.
    
    Change-Id: Ibe45d7facc452784541c2b7e2b4320b885c0ee95
    Signed-off-by: arnoldo.lutz.guevara@hpe.com <arnoldo.lutz.guevara@hpe.com>

---
+++ run-ovs-tests.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Copyright (C) 2016 Hewlett-Packard Development Company, L.P.
+# All Rights Reserved.
+#
+
+OVSTESTDIR="/ovstest"
+DOCKERIMAGE="ops-ovs-test"
+JEMALLOC=0
+YAML=0
+JEMALLOC=$(ldconfig -p | grep -c jemalloc)
+YAML=$(ldconfig -p | grep -c yaml)
+if [ $1 ]; then
+    YAML=0
+fi
+
+if [ ${JEMALLOC} -gt 0 ] && [ ${YAML} -gt 0 ]; then
+    echo "Running on local host"
+    ./ovs-test.sh;
+else
+    echo "One of jemalloc or yaml libraries not found, running on docker"
+    echo "Loading docker"
+# Need to add verification that the docker image is available. If not,
+# download the image from a store  place to be defined.
+    docker run --rm -h test  -v $(pwd):${OVSTESTDIR} --name test ${DOCKERIMAGE} &
+    sleep 2
+    echo "Waiting docker to be ready"
+    until [ "`/usr/bin/docker inspect -f {{.State.Running}} test`"=="true" ]; do
+        sleep 0.1;
+    done;
+    echo "Configuring options in docker"
+    docker exec -it test groupadd -g $(id -g ${USER}) $(stat -c "%G" .)
+    docker exec -it test useradd  -u $(id -u ${USER}) ${USER}
+    echo "Running tests in docker"
+    docker exec -ti test /bin/sh -c "cd ${OVSTESTDIR} && ./ovs-test.sh"
+    docker stop test
+fi
