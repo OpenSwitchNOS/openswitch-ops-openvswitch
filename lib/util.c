ca319ef2138f22c369885c4a5be77b19c25c9948
Cihangir Akyol <cihangir.m.akyol@hpe.com>
2016-05-13 13:56:40 -0700

    TG-1096 In preparation for passing fortify, the additional function xstrndup is needed.  This commit adds this utility function.
    Change-Id: I8f512002d727d1dc2ac15cd031ffc201b58a7b6c
    Signed-off-by: Cihangir Akyol <cihangir.m.akyol@hpe.com>

--- lib/util.c
+++ lib/util.c
@@ -152,6 +152,16 @@ xstrdup(const char *s)
 }
 
 char *
+xstrndup(const char *s, size_t len)
+{
+    int slen = strlen(s);
+
+    if (slen < len) len = slen;
+    return xmemdup0(s, len);
+
+}
+
+char *
 xvasprintf(const char *format, va_list args)
 {
     va_list args2;
